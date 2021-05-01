5c6e05378d1345f8cfdca7d0ddf1ac4fac86716f
Max-Domashuk <maxdomashuk@csus.edu>
2021-04-30 23:49:16 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -15,16 +15,10 @@ function App() {
   const [SEX, setSex] = useState("");
   const [HOSPITAL_NAME, setHospital] = useState("");
 
-  // Employee
-  const [EID, setEid] = useState("");
-  const [SALARY, setSalary] = useState("");
-  const [BIRTHDATE, setBirthdate] = useState("");
-  const [HOSP_NAME, setHosp] = useState("");
-
-
+  
   const[hospitalList, setHospitalList] = useState([]);
   const[patientList, setPatientList] = useState([]);
-  const[employeeList, setEmployeeList] = useState([]);
+  
  
 
   const addHospital = () => {
@@ -49,16 +43,6 @@ function App() {
     });
   };
 
-  const addEmployee = () => {
-    Axios.post("https://csc174-hospital.herokuapp.com/createEmployee", {
-      EID: EID, 
-      SALARY: SALARY, 
-      BIRTHDATE: BIRTHDATE,
-      HOSP_NAME: HOSP_NAME,
-    }).then(() => {
-      console.log("Success");
-    });
-  };
 
 const getHospitals = () => {
   Axios.get("https://csc174-hospital.herokuapp.com/Hospitals").then((response) => {
@@ -70,16 +54,12 @@ const getPatients= () => {
     setPatientList(response.data);
   });
 };
-const getEmployees= () => {
-  Axios.get("https://csc174-hospital.herokuapp.com/Employees").then((response) => {
-    setEmployeeList(response.data);
-  });
-};
+
 
   return (
   <div className="App">
     <div className="information">
-    <label>NAME:</label>
+    <label>NAME (HOSPITAL NAME):</label>
     <input 
     type="text"
     onChange={(event) => {
@@ -102,6 +82,23 @@ const getEmployees= () => {
      />
     <button onClick={addHospital}>Add Hospital</button>
     </div>
+
+    <div className="Hospitals">
+  <button onClick={getHospitals}>Show Hospitals</button>
+
+  {hospitalList.map((val,key) => {
+    return(
+      <div className="Hosp">
+       <h3>Hospital Table - </h3>
+       <h3>Name: {val.NAME}</h3>
+       <h3>Address: {val.ADDRESS}</h3>
+       <h3>City: {val.CITY}</h3>
+      </div>
+  );
+})}
+    </div>
+
+  
 
     <div className="patientInfo">
     <label>PID:</label>
@@ -132,7 +129,7 @@ const getEmployees= () => {
       setSex(event.target.value);
     }}
      />
-     <label>HOSPITAL_NAME:</label>
+     <label>HOSPITAL_NAME (Must reference hospital name entered above):</label>
     <input 
     type="text"
     onChange={(event) => {
@@ -142,20 +139,6 @@ const getEmployees= () => {
     <button onClick={addPatient}>Add Patient</button>
     </div> 
   
- <div className="Hospitals">
-  <button onClick={getHospitals}>Show Hospitals</button>
-
-  {hospitalList.map((val,key) => {
-    return(
-      <div className="Hosp">
-       <h3>Hospital Table - </h3>
-       <h3>Name: {val.NAME}</h3>
-       <h3>Address: {val.ADDRESS}</h3>
-       <h3>City: {val.CITY}</h3>
-      </div>
-  );
-})}
-    </div>
 
     <div className="Patients">
   <button onClick={getPatients}>Show Patients</button>
@@ -173,55 +156,7 @@ const getEmployees= () => {
   );
 })}
     </div>
-
-
-    <div className="employeeInfo">
-    <label>EID:</label>
-    <input 
-    type="text"
-    onChange={(event) => {
-      setEid(event.target.value);
-    }}
-     />
-    <label>SALARY:</label>
-    <input 
-    type="text"
-    onChange={(event) => {
-      setSalary(event.target.value);
-    }}
-     />
-    <label>BIRTHDATE:</label>
-    <input 
-    type="text"
-    onChange={(event) => {
-      setBirthdate(event.target.value);
-    }}
-     />
-     <label>HOSPITAL_NAME:</label>
-    <input 
-    type="text"
-    onChange={(event) => {
-      setHosp(event.target.value);
-    }}
-     />
-    <button onClick={addEmployee}>Add Employee</button>
-    </div> 
-  
- <div className="Employees">
-  <button onClick={getEmployees}>Show Employees</button>
-
-  {employeeList.map((val,key) => {
-    return(
-      <div className="Emp">
-       <h3>Employee Table - </h3>
-       <h3>EID: {val.EID}</h3>
-       <h3>Salary: {val.SALARY}</h3>
-       <h3>Birthdate: {val.BIRTHDATE}</h3>
-       <h3>Hospital Name: {val.HOSP_NAME}</h3>
-      </div>
-  );
-})}
-    </div>
+    
 
   </div>
   );
